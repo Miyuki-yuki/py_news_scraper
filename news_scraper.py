7e4b9b3608ff602ddfc17c7e02f18ddb0ab39513
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-11-04 23:15:37 +0900

    Fixed the problems

--- news_scraper.py
+++ news_scraper.py
@@ -1,26 +1,31 @@
 # filename: news_scraper.p
 import sys
-from bs4 import BeautifulSoup
-import requests
+import feedparser
 from datetime import datetime
 
 def scrape_news(start_date, end_date):
-    url = "https://www.bbc.com/news"  # replace with the URL of the news website you want to scrape
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    url = "http://feeds.bbci.co.uk/news/rss.xml"  # BBC News RSS feed
 
-    for news in soup.find_all('div', class_='news'):  # replace with the appropriate HTML tags and classes
-        headline = news.find('h2').text
-        source = news.find('p', class_='source').text
-        publication_date = datetime.strptime(news.find('p', class_='date').text, '%Y-%m-%d')
+    feed = feedparser.parse(url)
 
+    for entry in feed.entries:
+        publication_date = datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %Z')
         if start_date <= publication_date <= end_date:
-            print(f'Headline: {headline}')
-            print(f'Source: {source}')
+            print(f'Headline: {entry.title}')
+            print(f'Link: {entry.link}')
             print(f'Publication Date: {publication_date.strftime("%Y-%m-%d")}')
             print()
 
 if __name__ == "__main__":
-    start_date = datetime.strptime(sys.argv[1], '%Y-%m-%d') if len(sys.argv) > 1 else datetime.now()
-    end_date = datetime.strptime(sys.argv[2], '%Y-%m-%d') if len(sys.argv) > 2 else datetime.now()
+    if len(sys.argv) > 2:
+        try:
+            start_date = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+            end_date = datetime.strptime(sys.argv[2], '%Y-%m-%d')
+        except ValueError:
+            print("Invalid date format. Please provide dates in YYYY-MM-DD format.")
+            sys.exit(1)
+    else:
+        start_date = datetime.now()
+        end_date = datetime.now()
+
     scrape_news(start_date, end_date)
